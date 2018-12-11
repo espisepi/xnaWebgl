098ae0548a634f46d93d6fff0e6a5d8897d2e8fd
Unknown <sepinaco@gmail.com>
2018-12-11 22:59:45 +0100

    feat: añadida camera en indexSepinaco de forma temporal
    el codigo es un poco espaguetti pero se cambiara mas adelante

--- js/indexSepinaco.js
+++ js/indexSepinaco.js
@@ -8,6 +8,17 @@ document.addEventListener('DOMContentLoaded', function () {
     program = createProgram();
     cubeModel = new GameObject(program, [0, 0, -10], 'data/cube.json', program.textures[0]);
 
+    //Codigo temporal para la camara y animaciones
+    currentlyPressedKeys = {};
+    pitchRate = 0;
+    yawRate = 0;
+    speed = 0;
+    lastTime = 0;
+    joggingAngle = 0;
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+    //Fin de codigo temporal
+
     var gl = graphicsDevice.gl;
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
@@ -58,9 +69,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function tick() {
     requestAnimFrame(tick);
-    //handleKeys();
+    handleKeys();
     drawScene(graphicsDevice);
-    //animate();
+    animate();
 
     var gl = graphicsDevice.gl;
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -68,143 +79,78 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 
+  //Codigo para mover la camara
 
 
-  //   var program = new Zia.BasicProgram(graphicsDevice, {
-  //     lightingEnabled: true
-  //   });
-  //   program.enableDefaultLighting();
+  function handleKeyDown(event) {
+    currentlyPressedKeys[event.keyCode] = true;
+  }
 
-  //   var texture = Zia.Texture2D.createFromImagePath(graphicsDevice,
-  //     '../assets/textures/UV_Grid_Sm.jpg');
+  function handleKeyUp(event) {
+    currentlyPressedKeys[event.keyCode] = false;
+  }
 
-  //   var cubeModel = Zia.GeometricPrimitive.convertToModel(
-  //     graphicsDevice, Zia.GeometricPrimitive.createCube());
+  function handleKeys() {
+    if (currentlyPressedKeys[33]) {
+      // Page Up
+      pitchRate = 0.1;
+    } else if (currentlyPressedKeys[34]) {
+      // Page Down
+      pitchRate = -0.1;
+    } else {
+      pitchRate = 0;
+    }
 
-  //   var projectionMatrix = Zia.Matrix4.createPerspectiveFieldOfView(
-  //     Zia.MathUtil.PI_OVER_FOUR,
-  //     graphicsDevice.viewport.aspectRatio, 0.1, 100,
-  //     new Zia.Matrix4());
+    if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
+      // Left cursor key or A
+      yawRate = 0.1;
+    } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
+      // Right cursor key or D
+      yawRate = -0.1;
+    } else {
+      yawRate = 0;
+    }
 
-  //   var viewMatrix = Zia.Matrix4.createLookAt(
-  //     new Zia.Vector3(1, 1, -1),
-  //     new Zia.Vector3(0, 0, 0),
-  //     new Zia.Vector3(0, 1, 0),
-  //     new Zia.Matrix4());
+    if (currentlyPressedKeys[81]) {
+      camera.position[1] += 0.5;
+    } else if (currentlyPressedKeys[69]) {
+      camera.position[1] += -0.5;
+    }
 
-  //   var modelMatrix = Zia.Matrix4.createIdentity(new Zia.Matrix4());
+    if (currentlyPressedKeys[38] || currentlyPressedKeys[87]) {
+      // Up cursor key or W
+      speed = 0.01;
+    } else if (currentlyPressedKeys[40] || currentlyPressedKeys[83]) {
+      // Down cursor key
+      speed = -0.01;
+    } else {
+      speed = 0;
+    }
 
-  //   var lastCubeUpdateTime, rotationAngle = 0;
+  }
 
-  //   var stats = new Stats();
-  //   stats.domElement.style.position = 'absolute';
-  //   stats.domElement.style.top = '0px';
-  //   canvas.parentElement.appendChild(stats.domElement);
-
-  //   function drawScene() {
-  //     stats.begin();
-
-  //     if (graphicsDevice.resize()) {
-  //       Zia.Matrix4.createPerspectiveFieldOfView(
-  //         Zia.MathUtil.PI_OVER_FOUR,
-  //         graphicsDevice.viewport.aspectRatio,
-  //         0.1, 100,
-  //         projectionMatrix);
-  //     }
-
-  //     graphicsDevice.clear(
-  //       Zia.ClearOptions.ColorBuffer | Zia.ClearOptions.DepthBuffer,
-  //       new Zia.Color4(0.4, 0.4, 0.4, 1), 1);
-
-  //     Zia.Matrix4.createRotationY(
-  //       degToRad(rotationAngle),
-  //       modelMatrix);
-  //     cubeModel.draw(modelMatrix, viewMatrix, projectionMatrix);
-
-  //     var currentTime = (new Date).getTime();
-  //     if (lastCubeUpdateTime) {
-  //       var delta = currentTime - lastCubeUpdateTime;
-  //       rotationAngle += (10 * delta) / 1000.0;
-  //     }
-
-  //     lastCubeUpdateTime = currentTime;
-
-  //     stats.end();
-
-  //     requestAnimationFrame(drawScene);
-  //   }
-
-  //   function vector3ToColorArray(v) {
-  //     return v.toArray().map(function(x) {
-  //       return x * 255.0;
-  //     });
-  //   }
-
-  //   function colorArrayToVector3(c) {
-  //     return new Zia.Vector3(
-  //       c[0] / 255.0,
-  //       c[1] / 255.0,
-  //       c[2] / 255.0);
-  //   }
-
-  //   var controls = {
-  //     textureEnabled: true,
-  //     lightingEnabled: true,
-  //     perPixelLightingEnabled: true,
-
-  //     diffuseColor: vector3ToColorArray(program.diffuseColor),
-  //     specularColor: vector3ToColorArray(program.specularColor),
-  //     specularPower: program.specularPower,
-  //     emissiveColor: vector3ToColorArray(program.emissiveColor),
-  //     ambientLightColor: vector3ToColorArray(program.ambientLightColor),
-
-  //     toggleFullScreen: function() {
-  //       Zia.HtmlUtil.toggleFullScreen(canvas.parentElement);
-  //     }
-  //   };
+  //Codigo para animar la escena
+  // Used to make us "jog" up and down as we move forward.
 
 
-  //   var gui = new dat.GUI({ autoPlace: false });
-  //   canvas.parentElement.appendChild(gui.domElement);
+  function animate() {
+    var timeNow = new Date().getTime();
+    if (lastTime != 0) {
+      var elapsed = timeNow - lastTime;
 
-  //   var f1 = gui.addFolder('Basic parameters');
-  //   f1.add(controls, 'textureEnabled').
-  //     name('Texture').
-  //     onChange(createProgram);
-  //   f1.add(controls, 'lightingEnabled').
-  //     name('Lighting').
-  //     onChange(createProgram);
-  //   f1.add(controls, 'perPixelLightingEnabled').
-  //     name('Per-pixel lighting').
-  //     onChange(createProgram);
+      if (speed != 0) {
+        camera.position[0] -= Math.sin(MathUtils.degToRad(camera.yaw)) * speed * elapsed;
+        camera.position[2] -= Math.cos(MathUtils.degToRad(camera.yaw)) * speed * elapsed;
 
-  //   var f2 = gui.addFolder('Colors');
-  //   f2.addColor(controls, 'diffuseColor').
-  //     name('Diffuse color').
-  //     onChange(createProgram);
-  //   f2.addColor(controls, 'specularColor').
-  //     name('Specular color').
-  //     onChange(createProgram);
-  //   f2.add(controls, 'specularPower', 1, 64).
-  //     name('Specular power').
-  //     onChange(createProgram);
-  //   f2.addColor(controls, 'emissiveColor').
-  //     name('Emissive color').
-  //     onChange(createProgram);
+        //joggingAngle += elapsed * 0.6; // 0.6 "fiddle factor" - makes it feel more realistic :-)
+        //yPos = Math.sin(degToRad(joggingAngle)) / 20 + 0.4
+      }
 
-  //   var f3 = gui.addFolder('Lighting');
-  //   f3.addColor(controls, 'ambientLightColor').
-  //     name('Ambient light color').
-  //     onChange(createProgram);
+      camera.yaw += yawRate * elapsed;
+      camera.pitch += pitchRate * elapsed;
 
-  //   var f4 = gui.addFolder('Misc');
-  //   f4.add(controls, 'toggleFullScreen').name('Fullscreen');
-  //   f4.open();
-
-  //   requestAnimationFrame(drawScene);
+    }
+    lastTime = timeNow;
+  }
 
 }, false);
-
-// function degToRad(degree){
-//   return degree * (Math.PI/180);
-//  }
